a6521bef23b4edd4211247e330a963bb26f2bbb2
DawidKirsten <Sensability@gmail.com>
2015-05-24 11:17:40 +0200

    Assignment2
    Completed work for Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Making use of the example "makeVetor" and "cachemean" from the assignment page and altering it to calculate the inverse of a Matrix instead of the mean of a vector.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## Same as the "makeVector" with the x = numeric() replaced with x = matrix() so that our input for the function is now a matrix and not a vector 
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInvM <- function(inverse) m <<- inverse
+  ## Changed mean to inverse
+  getInvM <- function() m
+  list(set = set, get = get,
+       ## Change sub names from mean to (Inv)erse of (M)atrix
+       setInvM = setInvM,
+       getInvM = getInvM)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  ## Same as the "cachemean" function
+  ## Call new naming from "makeCacheMatrix"
+  m <- x$getInvM()
+  if ( ! is.null(m)) {
+    print("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  ## Swap the mean standard function with the solve standard function as per the instructions in assignment for computing the inverse of a square matrix
+  m <- solve(data)
+  x$setInvM(m)
+  m
 }
